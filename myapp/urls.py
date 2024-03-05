07837b042a2ba6191de7aad315c7fd6df6b3bdef
aditya-ptl-27 <adityaptl2711@gmail.com>
2024-03-05 13:54:21 +0530

    added forgot password feature

--- myapp/urls.py
+++ myapp/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     # path('lender_index/',views.lender_index,name='lender_index'),
     path('profile/',views.profile,name='profile'),
     path('change_password/',views.change_password,name='change_password'),
+    path('forgot_password/',views.forgot_password,name='forgot_password'),
+    path('verify_otp/',views.verify_otp,name='verify_otp'),
+    path('update_password/',views.update_password,name='update_password'),
 ]
