d24a7a85d46c2ddc48ea76bd7f93c06f344ab53c
aditya-ptl-27 <adityaptl2711@gmail.com>
2024-03-05 12:32:35 +0530

    Added change password feature

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),
-    path('login',views.login,name='login'),
-    path('signup',views.signup,name='signup'),
-    path('logout',views.logout,name='logout'),
-    path('logout',views.logout,name='logout'),
-    # path('lender_index',views.lender_index,name='lender_index'),
-    path('profile',views.profile,name='profile'),
+    path('login/',views.login,name='login'),
+    path('signup/',views.signup,name='signup'),
+    path('logout/',views.logout,name='logout'),
+    path('logout/',views.logout,name='logout'),
+    # path('lender_index/',views.lender_index,name='lender_index'),
+    path('profile/',views.profile,name='profile'),
+    path('change_password/',views.change_password,name='change_password'),
 ]
